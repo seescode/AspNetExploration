f83ecc617d345f65ae620102e14f7ce3e940a887
seescode <seesyong@gmail.com>
2015-10-09 13:56:48 -0400

    Created claims cookie

---
+++ LoginAnalysis/LoginAnalysis/App_Start/Startup.Auth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace LoginAnalysis
+{
+	public partial class Startup
+	{
+		public void ConfigureAuth(IAppBuilder app)
+		{
+			
+			// Enable the application to use a cookie to store information for the signed in user
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				CookieName = "atom.web",
+				LoginPath = new PathString("/home/index"),
+				ExpireTimeSpan = new System.TimeSpan(24, 0, 0),
+				SlidingExpiration = true
+			});
+	
+		}
+	}
+}
---
+++ LoginAnalysis/LoginAnalysis/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace LoginAnalysis.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+		[HttpGet]
+		[AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public ActionResult Login()
+		{
+			SignIn();
+
+			return View("Index");
+		}
+
+
+		private void SignIn()
+		{
+			var identity = new ClaimsIdentity(new[]
+				{
+					new Claim(ClaimTypes.Name, "Yong") 
+				},
+				DefaultAuthenticationTypes.ApplicationCookie,
+				ClaimTypes.Name,
+				ClaimTypes.Role);
+
+			//In order to get access to GetOwinContext I had to nuget install Microsoft.Owin.Host.SystemWeb	
+			HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false}, identity);
+		}
+    }
+}
